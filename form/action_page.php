bcf44b325a23734ddfb260b9801a9e99ca3e598f
nasimul007 <39893392+nasimul007@users.noreply.github.com>
2018-06-11 09:46:44 +0600

    Add files via upload

---
+++ form/action_page.php
@@ -0,0 +1,13 @@
+<?php
+
+	$uname = $_GET['uname'];
+	$pass = $_GET['pass'];
+
+	echo  "<html>";
+		echo "<body>";
+					echo "<h1>Name: ".$uname. "</h1>";
+					echo "<h1>Password:" .$pass."</h1>";
+		echo "</body>";
+	echo "</html>";
+
+?>
